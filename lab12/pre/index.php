0b77022a17f41ef98a61da2d383a18ada7c7a843
tae13152 <65070089@kmitl.ac.th>
2024-03-08 09:22:09 +0700

    Added Qrcode

---
+++ lab12/pre/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab12</title>
+</head>
+<body>
+
+<form method="post">
+    Enter QR Code Data:
+    <input type="text" name="str1"><br/>
+    <input type="submit" name="Submit"><br/>
+</form>
+
+<?php
+if (isset($_POST["Submit"])) {
+    $data = $_POST["str1"];
+    echo "<h2>Here is your QR Code<h2>";
+    echo <<<EOF
+<img src='https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=$data'>
+
+EOF;
+
+}
+?>
+
+</body>
+</html>
