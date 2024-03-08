6d5f623b9320eaa63a0e84e71965b669249ac3df
tae13152 <65070089@kmitl.ac.th>
2024-03-08 10:57:22 +0700

    Added Chart

--- lab12/3/process.php
+++ lab12/3/process.php
@@ -9,15 +9,3 @@ function convert_json($data) {
     }
     return $result;
 }
-
-$url = "http://10.0.15.21/lab/lab12/restapis/products.php?list=10";
-$response = file_get_contents($url);
-$dataPoints = json_decode($response);
-
-//{
-//    "ProductID": "1",
-//    "ProductCode": "B-1",
-//    "ProductName": "Chai",
-//    "Description": "Black tea with a blend of cardamom, ginger, cloves and cinnamon",
-//    "UnitPrice": 18
-//  },
