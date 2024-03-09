3853c3d487c44fc8eb3b712e2e4b41855171133c
tae13152 <65070089@kmitl.ac.th>
2024-03-09 17:33:28 +0700

    Completed Lab 12-1

--- lab12/1/index.php
+++ lab12/1/index.php
@@ -43,53 +43,63 @@ $result_data = $result[$index]
 
 
 <div class="" id="body_container" style="padding-top: 54px !important">
-    <div id="intro" class="mt-4 flex-row d-flex flex-grow flex-wrap gap-4">
+    <?php
+    function print_array($array) {
+        foreach ($array as $item) {
+            echo $item . ' ';
+        }
+    }
+
+    $url = "http://10.0.15.21/lab/lab12/restapis/10countries.json";
+    $response = file_get_contents($url);
+    $result = json_decode($response);
+
+    foreach ($result as $country) {
+        echo <<<EOF
+<div id="intro" class="mt-4 flex-row d-flex flex-grow flex-wrap gap-4">
         <div class="quiz-container blur-effect country_box">
-            <h1 class="text-center">Brazil</h1>
-            <div class="d-flex flex-row gap-3 justify-content-between">
-                <div>
-                    <div><strong>Capital: </strong>Brass</div>
-                    <div><strong>Population: </strong>Brass</div>
-                    <div><strong>Regions: </strong>Brass</div>
-                    <div><strong>Location: </strong>Brass</div>
-                    <div><strong>Borders: </strong>Brass</div>
-                </div>
-                <div>
-                    <img class="rounded" src="https://www.it.kmitl.ac.th/~sooksan/sooksan.jpg" alt="">
-                </div>
-            </div>
+EOF;
+
+        echo '<h1 class="text-center">' . $country->name . '</h1>';
+        echo '<div class="d-flex flex-row gap-3 justify-content-between">';
+        echo '<div>';
+        echo '<div><strong>Capital: </strong>' . $country->capital . '</div>';
+        echo '<div><strong>Population: </strong>' . number_format($country->population) . '</div>';
+        echo '<div><strong>Regions: </strong>' . $country->region . '</div>';
+        echo '<div><strong>Location: </strong>' . $country->latlng[0] . ', ' . $country->latlng[1] . '</div>';
+        echo '<div><strong>Border: </strong>';
+        print_array($country->borders);
+        echo '</div>';
+        echo <<<EOF
+</div>
+        <div>
+            <img class="rounded" src="$country->flag" alt="" width="400">
         </div>
-        <div class="quiz-container blur-effect country_box">
-            <h1 class="text-center">Brazil</h1>
-            <div class="d-flex flex-row gap-3 justify-content-between">
-                <div>
-                    <div><strong>Capital: </strong>Brass</div>
-                    <div><strong>Population: </strong>Brass</div>
-                    <div><strong>Regions: </strong>Brass</div>
-                    <div><strong>Location: </strong>Brass</div>
-                    <div><strong>Borders: </strong>Brass</div>
-                </div>
-                <div>
-                    <img class="rounded" src="https://www.it.kmitl.ac.th/~sooksan/sooksan.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="quiz-container blur-effect country_box">
-            <h1 class="text-center">Brazil</h1>
-            <div class="d-flex flex-row gap-3 justify-content-between">
-                <div>
-                    <div><strong>Capital: </strong>Brass</div>
-                    <div><strong>Population: </strong>Brass</div>
-                    <div><strong>Regions: </strong>Brass</div>
-                    <div><strong>Location: </strong>Brass</div>
-                    <div><strong>Borders: </strong>Brass</div>
-                </div>
-                <div>
-                    <img class="rounded" src="https://www.it.kmitl.ac.th/~sooksan/sooksan.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
+</div>
+</div>
+EOF;
+
+    }
+
+    ?>
+    <!--    <div id="intro" class="mt-4 flex-row d-flex flex-grow flex-wrap gap-4">-->
+    <!--        <div class="quiz-container blur-effect country_box">-->
+    <!--            <h1 class="text-center">Brazil</h1>-->
+    <!--            <div class="d-flex flex-row gap-3 justify-content-between">-->
+    <!--                <div>-->
+    <!--                    <div><strong>Capital: </strong>Brass</div>-->
+    <!--                    <div><strong>Population: </strong>Brass</div>-->
+    <!--                    <div><strong>Regions: </strong>Brass</div>-->
+    <!--                    <div><strong>Location: </strong>Brass</div>-->
+    <!--                    <div><strong>Borders: </strong>Brass</div>-->
+    <!--                </div>-->
+    <!--                <div>-->
+    <!--                    <img class="rounded" src="https://www.it.kmitl.ac.th/~sooksan/sooksan.jpg" alt="">-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
 </div>
 
 <div align="right" class="emailspin" style="color: white"><img alt="" border="0" src=" ../assets/emailspin.gif"><br>
